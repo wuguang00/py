93acb66f85cae0da545fd57ec518c5e63764e313
wuguang <wuguang@wuguangs-MacBook-Pro.local>
2019-10-10 23:19:09 +0800

    regularly updating

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # py
+# 
 I want to used this repository for store my python scripts
 Do one day one push.
+# install software using source codes
+# ./config
+# make 
+# sudo make install
---
+++ sys_argv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+####################################################################################################
+# For examaple: ./sys_argv.py -t a -g b
+####################################################################################################
+import sys
+print("sys.argv[0]: basename, sys.argv[1]: first parameter, sys.argv[2]: the second parameter, ...")
+print("You can also treat as an array.")
+print("sys.argv[0]: " + sys.argv[0] + ". The basename of the executable file.")
+print("sys.argv[1]: " + sys.argv[1])
+print("sys.argv[2]: " + sys.argv[2])
+print("sys.argv[3]: " + sys.argv[3])
+print("sys.argv[4]: " + sys.argv[4])
+print("sys.ps2: " + sys.PS2)
+
+
+
